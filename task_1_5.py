219e79f8baf8a6d6f682bb2bb7576ffc71759cec
ap <avpentyak@gmail.com>
2021-12-01 18:22:15 +0300

    fixed script for homework task 5

--- task_1_5.py
+++ task_1_5.py
@@ -1,2 +1,7 @@
-triangle_leg_1 = int(input("Entep first leg: "))
-triangle_leg_2 = int(input())
+triangle_leg_1 = int(input("Enter first leg: "))
+triangle_leg_2 = int(input("Enter second leg: "))
+hypotenuse_squared = (triangle_leg_1 ** 2) + (triangle_leg_2 ** 2)
+hypotenuse_length = hypotenuse_squared ** 0.5
+print(hypotenuse_length)
+triangle_area = (triangle_leg_1 * triangle_leg_2) * 0.5
+print(triangle_area)
