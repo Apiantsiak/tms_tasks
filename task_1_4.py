86e884dce7203583cb1cad439a95f493d2588dfd
ap <avpentyak@gmail.com>
2021-12-01 18:02:27 +0300

    add script for homework task 4

---
+++ task_1_4.py
@@ -0,0 +1,6 @@
+numb_1 = int(input("Enter numb 1: "))
+numb_2 = int(input("Enter numb 2: "))
+average_of_numbs = (numb_1 + numb_2) / 2
+print(average_of_numbs)
+geometric_mean_of_numbs = (numb_1 * numb_2) ** 0.5
+print(geometric_mean_of_numbs)
