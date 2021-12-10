44411586ce55b1fedff35917f4bf4aeb360120a1
Aliaksei Piantsiak <avpentyak@gmail.com>
2021-12-10 19:13:52 +0300

    Add file with code for task 3.10

---
+++ task_3_10.py
@@ -0,0 +1,21 @@
+# Receive the number of "рублей" and "копеек" for input and output it in the correct form,
+# for example, 3 "рубля", 1 "рубль" 25 "копеек", 3 "копейки".
+
+rubles, coins = int(input("Enter value of rubles: ")), int(input("Enter value of coins: "))
+
+rubles_ending: dict = {}
+coins_ending: dict = {}
+
+for numb in range(0, 100):
+    if numb == 1:
+        rubles_ending[numb] = "рубль"
+        coins_ending[numb] = "копейка"
+    elif numb % 10 in range(2, 5):
+        rubles_ending[numb] = "рубля"
+        coins_ending[numb] = "копейки"
+    else:
+        rubles_ending[numb] = "рублей"
+        coins_ending[numb] = "копеек"
+
+print("{} {} {} {}".format(rubles, rubles_ending[rubles], coins, coins_ending[coins]))
+
