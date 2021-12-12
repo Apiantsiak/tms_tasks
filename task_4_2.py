8dd272fa0269cd5a9ec813628bfa3272575813e1
Aliaksei Piantsiak <avpentyak@gmail.com>
2021-12-12 17:31:44 +0300

    Add file with code for task 4.2

---
+++ task_4_2.py
@@ -0,0 +1,24 @@
+# Дан список целых чисел. Подсчитать сколько четных чисел в списке
+
+# Using a for loop
+
+numbs: list = list(range(16))
+even_numbs_counter: int = 0
+
+for numb in numbs:
+    if numb % 2 == 0 and 0 < numb:
+        even_numbs_counter += 1
+
+print("Answer using a for loop:", even_numbs_counter)
+
+# Using a while loop
+
+even_numbs_counter: int = 0
+counter: int = 0
+
+while counter < len(numbs):
+    if numbs[counter] % 2 == 0 and numbs[counter] != 0:
+        even_numbs_counter += 1
+    counter += 1
+
+print("Answer using a while loop:", even_numbs_counter)
