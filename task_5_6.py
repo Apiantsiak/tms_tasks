3c0b13720843147f54f1a67b515dc986a6e1917d
Aliaksei Piantsiak <avpentyak@gmail.com>
2021-12-17 20:09:44 +0300

    Add code for task 5.6

--- task_5_6.py
+++ task_5_6.py
@@ -1,3 +1,22 @@
 # Задан целочисленный массив. Определить количество участков массива,
-# на котором элементы монотонно возрастают (каждое следующее число
-# больше предыдущего). [02-4.1-ML27]
+# на котором элементы монотонно возрастают (каждое следующее число больше предыдущего).
+
+from random import randint
+from typing import List
+
+rand_lst: List[int] = [randint(0, 20) for i in range(randint(0, 20))]
+result: int = 0
+count: int = 0
+
+print(rand_lst)
+
+for i in range(len(rand_lst) - 2):
+    if rand_lst[i + 2] > rand_lst[i + 1] > rand_lst[i]:
+        count += 1
+    elif count >= 1 and rand_lst[i + 1] > rand_lst[i + 2]:
+        result += 1
+        count = 0
+if rand_lst[-1] > rand_lst[-2] > rand_lst[-3]:
+    result += 1
+
+print(result)
