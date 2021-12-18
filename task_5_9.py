41ca009e13aa0e6b3057ea0c844058d77e061c46
Aliaksei Piantsiak <avpentyak@gmail.com>
2021-12-18 14:56:14 +0300

    Add code for task 5.9

--- task_5_9.py
+++ task_5_9.py
@@ -1,3 +1,25 @@
 # Для каждого натурального числа в промежутке от m до n вывести все делители,
 # кроме единицы и самого числа. m и n вводятся с клавиатуры.
 # Пример:m =100, n = 105
+
+from random import randint
+
+m: int = randint(2, 200)
+n: int = randint(2, 200)
+
+if m > n:
+    print(f"n = {n} m = {m}")
+    for numb in range(n, m + 1):
+        print()
+        print(numb, end=": ")
+        for var in range(2, numb - 1):
+            if numb % var == 0:
+                print(var, end=" ")
+elif n > m:
+    print(f"m = {m} n = {n}")
+    for numb in range(m, n + 1):
+        print()
+        print(numb, end=": ")
+        for var in range(2, numb - 1):
+            if numb % var == 0:
+                print(var, end=" ")
