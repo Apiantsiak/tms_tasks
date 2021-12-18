470c88a6c13631b40ae5236f139fb50acf341cc1
Aliaksei Piantsiak <avpentyak@gmail.com>
2021-12-18 19:11:06 +0300

    Add code for task 5.7

--- task_5_7.py
+++ task_5_7.py
@@ -1,3 +1,22 @@
 # Дана целочисленная квадратная матрица. Найти в каждой строке наи-
 # больший элемент и поменять его местами с элементом главной диагонали.
-# [02-4.2-ML22]
+
+from random import randint
+from typing import List
+
+high_matrix: int = 6
+length_matrix: int = 6
+
+matrix: List[List[int or str]] = [[randint(0, 100) for _ in range(high_matrix)]
+                                  for _ in range(length_matrix)
+                                  ]
+
+max_value_index: int = 0
+max_value: int = 0
+
+for i in matrix:
+    max_value = max(i)
+    i.remove(max(i))
+    i.insert(max_value_index, f"|{max_value}|")
+    max_value_index += 1
+    print(i, f"Max is {max_value}")
