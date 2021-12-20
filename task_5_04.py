66251d500862f133e3ad44fbb1f5995776c15f98
Aliaksei Piantsiak <avpentyak@gmail.com>
2021-12-20 15:29:11 +0300

    Add description for output

--- task_5_04.py
+++ task_5_04.py
@@ -9,12 +9,18 @@ n: int = randint(3, 10)
 m: int = randint(3, 10)
 
 A: List[List[int]] = [[randint(0, 10) for n in range(n)]for m in range(m)]
-avr_sum = 0
+avr_sum: float = sum(sum(i) for i in A) / (n * m)
+
+avr_ind_check: int = 0
+counter: int = 0
 
 for i in A:
-    avr_sum += sum(i)
-    print(i)
-    print(avr_sum / n * m)
-    for j in i:
-        if j > avr_sum / n*m:
-            print(j)
+    for j in range(len(i)):
+        if i[j] > avr_sum:
+            avr_ind_check += j
+            counter += 1
+
+if avr_ind_check % 2 == 0:
+    print(f"Number of elements {counter}")
+else:
+    print(f"The sum of the indices is not even and is equal to {avr_ind_check}")
