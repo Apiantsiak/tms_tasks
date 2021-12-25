5a6cb099ce75056a961d24ada16dfaa221067d4e
Aliaksei Piantsiak <avpentyak@gmail.com>
2021-12-25 17:18:47 +0300

    Add function description

--- task_7_06.py
+++ task_7_06.py
@@ -1,18 +1,10 @@
 # Написать функцию по решению квадратных уравнений.
-
-
-# Вычислить квадратное уравнение
-#
-# a(x**2) + bx + c = 0 (*)
-#
-# D = b**2 – 4ac;
-#
-# X1,2 = (-b +/- sqrt (D)) / 2a
 #
 # Предусмотреть 3 варианта:
 # 1) Два действительных корня
 # 2) Один действительный корень
 # 3) Нет действительных корней
+
 
 from typing import Union, NoReturn
 from math import sqrt
@@ -22,7 +14,13 @@ from random import uniform
 def quadratic_equation_func(a: Union[int, float],
                             b: Union[int, float],
                             c: Union[int, float]) -> NoReturn:
+    """
+    Calculate Quadratic Equation
 
+    a(x**2) + bx + c = 0 (*)
+    D = b**2 – 4ac;
+    X1,2 = (-b +/- sqrt (D)) / 2a
+    """
     disc_nant: float = b ** 2 - 4 * a * c
     print("Дискриминант D = {}".format(disc_nant))
 
