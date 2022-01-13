c5fc90b9bfbe5db20542a2f7b93305dac0a681c1
Aliaksei Piantsiak <avpentyak@gmail.com>
2022-01-13 17:23:03 +0300

    Add comments and exclude an optional part of the code

--- task_9_05.py
+++ task_9_05.py
@@ -4,7 +4,7 @@
 
 
 from typing import List
-from pathlib import Path
+# from pathlib import Path
 
 try:
     with open("test.txt") as text_file:
@@ -24,10 +24,17 @@ with open("test_9_05(1).txt", "w") as even_line_file:
 with open("test_9_05(2).txt", "w") as odd_line_file:
     odd_line_file.writelines(data_file[::2])
 
+# Just to try deleting file with help of Python
 
-if Path.is_file(Path("test.txt")):
-    del_files = input("Do you want to delete created files? y/n: ")
-    if del_files == "y":
-        Path("test.txt").unlink()
-        Path("test_9_05(1).txt").unlink()
-        Path("test_9_05(2).txt").unlink()
+# if Path.is_file(Path("test.txt")):
+#     del_files = input("Do you want to delete created files? y/n: ")
+#     if del_files == "y":
+#         Path("test.txt").unlink()
+#         Path("test_9_05(1).txt").unlink()
+#         Path("test_9_05(2).txt").unlink()
+#         Path("test_json.txt").unlink()
+#         Path("test_9_06.txt").unlink()
+#         Path("test_9_06(1).txt").unlink()
+#         Path("test_json(1).txt").unlink()
+#         Path("test_9_04.txt").unlink()
+#         Path("test_9_04(1).txt").unlink()
