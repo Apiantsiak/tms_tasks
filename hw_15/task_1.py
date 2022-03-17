d6fcf49df22e4e7b636de8c93e6742c6c91a8e0e
Aliaksei Piantsiak <avpentyak@gmail.com>
2022-03-17 22:01:42 +0300

    Refactor the code structure

--- hw_15/task_1.py
+++ hw_15/task_1.py
@@ -5,18 +5,39 @@
 
 
 from random import randint
+from typing import Dict
 
 
-while True:
+def get_random_number() -> None:
+    """Output for user random number
 
-    print("Do you need a randon number from 1 to 100?")
-    user_choice = input("If you need a number enter 'Y/y' or enter 'Q/q' to exit: ").lower()
+    :return: None
+    """
 
-    give_numb = user_choice == "y"
-    exit_loop = user_choice == "q"
+    text_for_user: Dict[str, str] = {'entry': 'Do you need a randon number from 1 to 100?',
+                                     'rule': 'If you need a number enter "Y" or enter "Q" to exit: ',
+                                     'next': 'Do you need next random number (Y/Q): ',
+                                     'result': f'This is your random number:',
+                                     'exit': 'Bye',
+                                     'error': 'Please enter "Y" or "Q": ',
+                                     }
+    print(text_for_user['entry'])
+    question = 'rule'
 
-    if give_numb:
-        print(f"\nThis is your random number: {randint(1, 100)}\n")
-    elif exit_loop:
-        print("\nBye\n")
-        break
+    while True:
+        user_answer = input(text_for_user[question]).lower()
+        give_numb = user_answer == 'y'
+        exit_loop = user_answer == 'q'
+        if give_numb:
+            rand_numb = randint(1, 100)
+            print(text_for_user['result'], rand_numb)
+            question = 'next'
+        elif exit_loop:
+            print(text_for_user['exit'])
+            break
+        else:
+            question = 'error'
+
+
+if __name__ == '__main__':
+    get_random_number()
