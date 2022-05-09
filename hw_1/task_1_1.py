b71a39b95358f630638b54b97b5117c0e8bc4db6
Aliaksei Piantsiak <avpentyak@gmail.com>
2022-05-09 17:59:32 +0300

    Change files structure

---
+++ hw_1/task_1_1.py
@@ -0,0 +1,7 @@
+# You are given 2 real numbers a and b. Get their sum, difference and product.
+
+
+a = int(input("Enter number a: "))
+b = int(input("Enter number b: "))
+
+print("Sum =", a+b, "Difference =", a-b, "Product =", a*b)
---
+++ hw_1/task_1_2.py
@@ -0,0 +1,9 @@
+# You are given real numbers x and y. Receive (x - y) / (1 + x*y)
+
+
+x = int(input("Enter numb x: "))
+y = int(input("Enter numb y: "))
+
+result_xy = (x-y) / (1 + x*y)
+
+print(result_xy)
