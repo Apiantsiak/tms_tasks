ff5e115d0b95dbb8348f8c59b7825a0d05a84e08
Aliaksei Piantsiak <avpentyak@gmail.com>
2022-05-13 19:14:45 +0300

    Change project settings and files structure

--- hw_23/my_django_project/my_django_project/settings.py
+++ hw_23/my_django_project/my_django_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'my_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 STATIC_URL = 'static/'
 
 # Default primary key field type
